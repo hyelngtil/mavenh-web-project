7641c1d6558cf291ec76e96614e5fcebf5945582
Hyelngtil <isaachyelngtil@gmail.com>
2026-08-20 08:42:33 +0000

    Introduce error to stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -7,3 +7,12 @@
 #if [[ -n $isExistApp ]]; then
 #sudo systemctl stop tomcat.service
 #fi
+
+# Add this at the top of your script to make it exit immediately if any command fails
+set -e
+
+# Your intentional error
+sudo systemctll stop httpd.service  # Misspelled command will now cause script to exit with non-zero status
+
+# To be extra sure, we can also add an explicit exit code
+exit 1
